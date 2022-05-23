d6d05aefc06342d499a3690d78ff1d13846112d2
kid <78607898+Kid1412-Jie@users.noreply.github.com>
2022-05-23 14:21:12 +0800

    Update test.java

--- src/main/java/com/example/demo/test.java
+++ src/main/java/com/example/demo/test.java
@@ -12,5 +12,9 @@ public class test {
         System.out.println("1111111");
         System.out.println("2222222");
         System.out.println("3333333");
+        System.out.println("=======");
+        System.out.println("1111111");
+        System.out.println("2222222");
+        System.out.println("3333333");
     }
 }
