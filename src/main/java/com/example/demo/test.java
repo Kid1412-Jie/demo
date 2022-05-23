68d3baabc11d857f29c541c562b06ae9b015e3ac
huangzj <Kid@jie.com>
2022-05-23 13:57:48 +0800

    33333333333

--- src/main/java/com/example/demo/test.java
+++ src/main/java/com/example/demo/test.java
@@ -9,14 +9,8 @@ package com.example.demo;
 public class test {
     public static void main(String[] args) {
         System.out.println("==================");
-        System.out.println("==================");
-        System.out.println("==================");
-        System.out.println("==================");
-        System.out.println("==================");
-        System.out.println("==================");
-        System.out.println("==================");
-        System.out.println("==================");
-        System.out.println("==================");
-        System.out.println("==================");
+        System.out.println("11111111111");
+        System.out.println("2222222222222");
+        System.out.println("3333333333");
     }
 }
