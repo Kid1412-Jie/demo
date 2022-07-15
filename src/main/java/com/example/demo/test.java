36bf15fb8c5ba26b462ab4c8a78cf58e813497bd
kid <78607898+Kid1412-Jie@users.noreply.github.com>
2022-07-15 09:39:24 +0800

    Update test.java
    git添加上去的

--- src/main/java/com/example/demo/test.java
+++ src/main/java/com/example/demo/test.java
@@ -9,5 +9,11 @@ package com.example.demo;
 public class test {
     public static void main(String[] args) {
         System.out.println("======="); 
+        System.out.println("======="); 
+        System.out.println("======="); 
+        System.out.println("======="); 
+        System.out.println("======="); 
+        System.out.println("======="); 
+        System.out.println("======="); 
     }
 }
