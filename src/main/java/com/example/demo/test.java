28c0d5ba6fdf1fc13f8cc1285256b06a1c781c72
kid <78607898+Kid1412-Jie@users.noreply.github.com>
2022-07-15 09:35:11 +0800

    Update test.java

--- src/main/java/com/example/demo/test.java
+++ src/main/java/com/example/demo/test.java
@@ -8,7 +8,6 @@ package com.example.demo;
  */
 public class test {
     public static void main(String[] args) {
-        System.out.println("=======");
-        
+        System.out.println("======="); 
     }
 }
