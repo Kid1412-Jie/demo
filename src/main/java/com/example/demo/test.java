5528804020cd26fcb91b32ddeb139f530932a931
kid <78607898+Kid1412-Jie@users.noreply.github.com>
2022-07-15 09:27:29 +0800

    Update test.java

--- src/main/java/com/example/demo/test.java
+++ src/main/java/com/example/demo/test.java
@@ -9,14 +9,6 @@ package com.example.demo;
 public class test {
     public static void main(String[] args) {
         System.out.println("=======");
-        System.out.println("1111111");
-        System.out.println("2222222");
-        System.out.println("3333333");
-        System.out.println("=======");
-        System.out.println("1111111");
-        System.out.println("2222222");
-        System.out.println("3333333");
-        System.out.println("3333333");
-        System.out.println("3333333");
+        
     }
 }
